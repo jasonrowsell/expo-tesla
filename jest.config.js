a0ddef89fc24ec0150f21a72dbc3148de1e1478e
jasonrowsell <jasonrowsell@hotmail.co.uk>
2021-08-28 09:53:49 +0100

    Adds setup for testing and linting

---
+++ jest.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  preset: 'jest-expo',
+
+  collectCoverage: true,
+
+  transformIgnorePatterns: [
+    'node_modules/(?!(jest-)?react-native|react-clone-referenced-element|@react-native-community|expo(nent)?|@expo(nent)?/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|@sentry/.*)',
+  ],
+
+  coverageReporters: [
+    'html',
+    'text',
+  ],
+};
